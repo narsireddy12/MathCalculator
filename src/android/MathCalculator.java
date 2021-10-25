58c650c8b58f5fdc3ef34bc1f0c18b7ac6890bc7
narsireddy <narsireddy.challa@magikminds.com>
2021-10-25 11:15:40 +0530

    minor fixes

--- src/android/MathCalculator.java
+++ src/android/MathCalculator.java
@@ -44,14 +44,14 @@ public class MathCalculator extends CordovaPlugin {
             try{
                 int p1 = Integer.paseInt(args.getJSONObject(0).getString("param1"));
                 int p2 = Integer.paseInt(args.getJSONObject(0).getString("param2"));
-                callback.success(""+ (p1+p2) )
+                callback.success(""+ (p1+p2) );
 
             }catch(Exception ex){
-                callback.error("Something went wrong " + ex)
+                callback.error("Something went wrong " + ex);
             }
 
         }else{
-            callback.error("Please donot pass null values")
+            callback.error("Please donot pass null values");
         }
 
     }
@@ -61,14 +61,14 @@ public class MathCalculator extends CordovaPlugin {
             try{
                 int p1 = Integer.paseInt(args.getJSONObject(0).getString("param1"));
                 int p2 = Integer.paseInt(args.getJSONObject(0).getString("param2"));
-                callback.success(""+ (p1-p2) )
+                callback.success(""+ (p1-p2) );
 
             }catch(Exception ex){
-                callback.error("Something went wrong " + ex)
+                callback.error("Something went wrong " + ex);
             }
 
         }else{
-            callback.error("Please donot pass null values")
+            callback.error("Please donot pass null values");
         }
 
     }
